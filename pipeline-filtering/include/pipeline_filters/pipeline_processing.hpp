b06e54c8d6ae4bde6ad4198efb60ad685df0d585
simonedn <127021988+simonedn@users.noreply.github.com>
2025-03-05 13:37:25 +0100

    Pipeline filtering (#4)
    * Updated with vscode stuff
    
    * Initial working setup for pipeline filtering
    
    * Minor changes, but breaking
    
    * Buildable checkpoint
    
    * Added grayscale weights
    
    * Added color channel weights
    
    * fix
    
    * changed name of launch file
    
    * changed folder name
    
    * removed unused dependency
    
    ---------
    
    Co-authored-by: Simon E <>
    Co-authored-by: Jorgen Fjermedal <jorgen.fjermedal@hotmail.com>

---
+++ pipeline-filtering/include/pipeline_filters/pipeline_processing.hpp
@@ -0,0 +1,53 @@
+#ifndef PIPELINE_PROCESSING_HPP
+#define PIPELINE_PROCESSING_HPP
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/xphoto.hpp>
+#include <iostream>
+#include <map>
+
+namespace vortex::pipeline_processing
+{
+
+
+struct OtsuParams {
+    bool gamma_auto_correction;
+    double gamma_auto_correction_weight;
+    bool otsu_segmentation;
+    double gsc_weight_r;
+    double gsc_weight_g;
+    double gsc_weight_b;
+};
+
+struct FilterParams {
+    OtsuParams otsu;
+};
+
+typedef void (*FilterFunction)(const FilterParams&, const cv::Mat&, cv::Mat&);
+
+/**
+ * Reads the filter_type from the FilterParams struct 
+ * and calls the appropriate filter function from the filter_functions map.
+ */
+void apply_filter(const std::string& filter, const FilterParams& params, const cv::Mat &original, cv::Mat &output);
+
+/**
+ * No filter, just copy the image
+ */
+void no_filter(const FilterParams& params, const cv::Mat &original, cv::Mat &output);
+
+/**
+ * A filter based on Otsu's method
+ */
+void otsu_segmentation_filter(const FilterParams& params, const cv::Mat &original, cv::Mat &output);
+
+const static std::map<std::string, FilterFunction> filter_functions ={
+	{"no_filter", no_filter},
+    {"otsu", otsu_segmentation_filter}};
+
+
+} // namespace pipeline_processing
+#endif // PIPELINE_PROCESSING_HPP
---
+++ pipeline-filtering/launch/pipeline_filtering.launch.py
@@ -0,0 +1,16 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pipeline_filtering_node = Node(
+            package='pipeline_filtering',
+            executable='pipeline_filtering_node',
+            name='pipeline_filtering_node',
+            parameters=[os.path.join(get_package_share_directory('pipeline_filtering'),'config','pipeline_filtering_params.yaml')],
+            output='screen',
+        )
+    return LaunchDescription([
+        pipeline_filtering_node
+    ])
